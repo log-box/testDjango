f6cabc6ccb8014cadce5e36c0b809e65ced717df
log-box <a.y.logvin@gmail.com>
2021-09-23 16:36:11 +0300

    Edited max_length in model "Questions" to the next fields: question 200/250 rightAnswer 50/70 aboutQuestion 1500/1900/2000 themeOfQuestion 50/90/130/150 questionCategory 50/90/110/120 sectionOfQuestion 30/55

--- quizapp/models.py
+++ quizapp/models.py
@@ -15,18 +15,14 @@ class Questions(models.Model):
     wrongAnswerThree = models.CharField("Первый неправильный ответ", max_length=50, blank=False)
     commentForJudge = models.CharField("Комментарий к вопросу", max_length=400, blank=False)
     aboutQuestion = models.CharField("Описание вопроса", max_length=2000, blank=False)
+    themeOfQuestion = models.CharField("Тема", max_length=150, blank=True, null=True)
     link = models.CharField("Источник информации", max_length=50, blank=False)
-    # themeOfQuestion = models.CharField("Тема", max_length=150, blank=False)
-    # themeOfQuestion = models.ForeignKey("QuestionsThemes", on_delete=models.PROTECT)
-    # questionCategory = models.CharField("Категория", max_length=120, blank=False)
     sectionOfQuestion = models.CharField("Раздел", max_length=55, blank=False)
     complexityOfQuestion = models.IntegerField("Сложность по десятибальной шкале", blank=False, null=True, default=0)
-
     approve = models.IntegerField("Статус одобрения", blank=False, default=0)
     date_ques_sub = models.DateField("Дата подачи вопроса", blank=False)
     base_date = models.DateField("Дата добавления в базу", blank=False, null=True)
 
-    # recursive = models.ForeignKey('self', on_delete=models.PROTECT)
 
     def save(self, *args, **kwargs):
         if not self.id:
@@ -34,28 +30,16 @@ class Questions(models.Model):
         return super(Questions, self).save(*args, **kwargs)
 
 
-# class QuestionsComments(models.Model):
-#     """Модель комментариев для вопросов"""
-#     # question = models.ForeignKey(Questions, on_delete=models.CASCADE)
-#     comment = models.CharField("Комментарий", max_length=400, blank=True)
-
-
 class QuestionsReviewer(models.Model):
-    # question = models.OneToOneField(Questions, on_delete=models.CASCADE, primary_key=True)
-    question = models.ManyToManyField(Questions)
+    question = models.OneToOneField(Questions, on_delete=models.CASCADE, primary_key=True)
     reviewer = models.CharField("Рецензент", max_length=30, blank=True, null=True)
     reviewer_date = models.DateField("Дата рецензии", blank=True)
 
 
-class QuestionsThemes(models.Model):
-    """Модель тем для вопросов"""
-    question = models.ManyToManyField(Questions)
-    themeOfQuestion = models.CharField("Тема", max_length=150, blank=True, null=True)
-    # themeCategory = models.ForeignKey(
-    #     "QuestionsThemesCategory",
-    #     null=True,
-    #     on_delete=models.PROTECT,
-    #     verbose_name='Категория')
+# class QuestionsThemes(models.Model):
+#     """Модель тем для вопросов"""
+#     # question = models.ForeignKey(Questions, on_delete=models.CASCADE, primary_key=True)
+#     question = models.ManyToManyField(Questions)
 
 
 
@@ -63,8 +47,6 @@ class QuestionsThemesCategory(models.Model):
     """Модель категорий для тем"""
     # themeOfQuestion = models.ForeignKey(QuestionsThemes, on_delete=models.CASCADE, blank=True, null=True)
     questionCategory = models.CharField("Категория", max_length=120)
-    questionTheme = models.ForeignKey(
-        "QuestionsThemes",
-        null=True,
-        on_delete=models.SET_NULL,
-        verbose_name='Тема')
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE, blank=True, null=True)
+
+
